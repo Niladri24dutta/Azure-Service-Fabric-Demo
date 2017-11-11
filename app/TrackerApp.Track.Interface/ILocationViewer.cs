e3f99af07eb3a739135cd864a04788616550c03f
Niladri Dutta <nild2141@gmail.com>
2017-11-11 20:55:28 +0000

    Add new stateful service and interfaces project

---
+++ app/TrackerApp.Track.Interface/ILocationViewer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerApp.Track.Interface
+{
+    public interface ILocationViewer : IService
+    {
+        Task<KeyValuePair<float, float>?> GetLastReportingLocation(Guid orderId);
+        Task<DateTime?> GetLastReportingTime(Guid orderId);
+    }
+}
